11102e78b9913364bc7aa420b46744fb646dc2d5
Ayush khatri <ayushkhatri362@gmail.com>
2024-04-10 15:27:27 +0530

    Made the initial template of the website

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import { MainSection } from "@/sections/mainSection";
+import Layout from "../components/layout";
+import React from "react";
+
+export default function Home() {
+    return (
+        <Layout>
+            <MainSection />
+        </Layout>
+    );
+}
---
+++ sections/mainSection.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const MainSection = () => {
+    return <div>main section</div>;
+};
